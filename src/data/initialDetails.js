4c843a5322b686fc18d6e155993ca46001877cb0
Dennis Mbugua <dennis@starlaunch.org>
2022-04-01 09:57:12 +0300

    React Search Feature

---
+++ src/data/initialDetails.js
@@ -0,0 +1,39 @@
+const initialDetails = [
+  {
+    id: 1,
+    imgPath: "/assets/img/1.png",
+    name: "Jane Doe",
+    email: "janedoe@gmail.com",
+    address: "Kigali, Rwanda",
+  },
+  {
+    id: 2,
+    imgPath: "/assets/img/2.png",
+    name: "Mary Rosamund",
+    email: "agra@rosie.com",
+    address: "Addis Ababa, Ethiopia",
+  },
+  {
+    id: 3,
+    imgPath: "/assets/img/3.png",
+    name: "Sherlock Watson",
+    email: "irene@johnlock.com",
+    address: "Dar es Salaam, TZ",
+  },
+  {
+    id: 4,
+    imgPath: "/assets/img/4.png",
+    name: "John Holmes",
+    email: "mary@johnlock.com",
+    address: "Biashara St. , Nairobi, Kenya",
+  },
+  {
+    id: 5,
+    imgPath: "/assets/img/5.png",
+    name: "Mycroft Lestrade",
+    email: "britishgovt@gmail.com",
+    address: "London, UK",
+  },
+];
+
+export default initialDetails;
